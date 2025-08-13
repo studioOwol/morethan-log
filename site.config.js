5255feba3188cbfc5bbf11e4d5af41f375df14a1
Nahyun <from.owol@gmail.com>
2025-08-13 22:59:36 +0900

    style: 프로필 사진 및 파비콘 변경

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "OWOL",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/notion-profile.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using javascript.",
     email: "from.owol@gmail.com",
