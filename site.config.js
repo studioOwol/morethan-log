55490487414b192e6099cc9edae77877ad81f2ba
Nahyun <from.owol@gmail.com>
2025-08-13 23:04:42 +0900

    chore: edit bio

--- site.config.js
+++ site.config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     name: "OWOL",
     image: "/notion-profile.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
-    bio: "I develop everything using javascript.",
+    bio: "Just Keep Programming 🐠",
     email: "from.owol@gmail.com",
     linkedin: "fromnahyun",
     github: "studioOwol",
